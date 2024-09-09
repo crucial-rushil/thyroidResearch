3ee3890e156e6384fa1f004a68a3bafc0f7a9af1
Rushil Shah <rushilshah2016@gmail.com>
2024-09-09 14:06:45 -0700

    last week changes

--- my-app/src/pages/calculate.js
+++ my-app/src/pages/calculate.js
@@ -3,28 +3,47 @@ export const calculateDosage = (hormone, value, bmi, gender) => {
     
     let dosage = 0;
     
-    if (hormone === 'TSH' && gender === 'Male' && bmi === "17") {
-      dosage = 0.008349 + Math.pow((2.332 * value),-1.547)
+    
+    if (hormone && gender === 'Male' && bmi === "17") {
+        const constant1 = -0.000233; 
+        const constant2 = 1.583;
+        const exponent = -0.05264 * (value - 57.15);
+        dosage = constant1 + (constant2 * Math.exp(-Math.exp(exponent)));
     } 
 
-    else if (hormone === 'TSH' && gender === 'Male' && bmi === "23") {
-        dosage = 0.008363 + Math.pow((2.578 * value),-1.543)
+    else if (hormone && gender === 'Male' && bmi === "23") {
+        const constant1 = -0.002095;
+        const constant2 = 1.676;
+        const exponent = -0.04886 * (value - 59.95);
+        dosage = constant1 + (constant2 * Math.exp(-Math.exp(exponent)));
     }
 
-    else if (hormone === 'TSH' && gender === 'Male' && bmi === "35") {
-        dosage = 0.008367 + Math.pow((2.949 * value),-1.538)
+    else if (hormone && gender === 'Male' && bmi === "35") {
+        const constant1 = -0.001712;
+        const constant2 = 1.668;
+        const exponent = -0.04831 * (value - 61.84);
+        dosage = constant1 + (constant2 * Math.exp(-Math.exp(exponent)));
     }
 
-    else if (hormone === 'TSH' && gender === 'Female' && bmi === "17") {
-        dosage = 0.008312 + Math.pow((1.926 * value),-1.551)
+    else if (hormone && gender === 'Female' && bmi === "17") {
+        const constant1 = -0.003621;
+        const constant2 = 1.789;
+        const exponent = -0.04747 * (value - 57.96);
+        dosage = constant1 + (constant2 * Math.exp(-Math.exp(exponent)));
     }
 
-    else if (hormone === 'TSH' && gender === 'Female' && bmi === "23") {
-        dosage = 0.008349 + Math.pow((2.168 * value),-1.547)
+    else if (hormone && gender === 'Female' && bmi === "23") {
+        const constant1 = -0.003232;
+        const constant2 = 1.774;
+        const exponent = -0.04696 * (value - 59.70);
+        dosage = constant1 + (constant2 * Math.exp(-Math.exp(exponent)));
     }
 
-    else if (hormone === 'TSH' && gender === 'Female' && bmi === "35") {
-        dosage = 0.008364 + Math.pow((2.542 * value),-1.541)
+    else if (hormone && gender === 'Female' && bmi === "35") {
+        const constant1 = -0.000277;
+        const constant2 = 1.759;
+        const exponent = -0.04635 * (value - 61.92);
+        dosage = constant1 + (constant2 * Math.exp(-Math.exp(exponent)));
     }
 
     return dosage
